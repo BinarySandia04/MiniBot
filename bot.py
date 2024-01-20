24db6ccd9bd87d7c07b173df5582df1ece6a67cd
guefusag2 <jhidalca91@alumnes.ub.edu>
2024-01-20 16:55:39 +0100

    He canviat el desto
    Opcional

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
-
+#juan haz el mini mini porfa :sob: :pray:
 # Crea una instancia del bot
 intents = discord.Intents.default()
 intents.message_content = True
